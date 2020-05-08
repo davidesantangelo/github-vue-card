7056f62dc0cd2905d29d4c99eaf7b723cc4513a2
Davide Santangelo <davide.santangelo@gmail.com>
2020-05-08 11:57:25 +0200

    Add top GitHub URL Alert

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="user">
+     <a v-bind:href="response.html_url" class="flex flex-col bg-orange-100 border-t-4 border-orange-500 text-orange-700 p-4">
+      <div class="text-center py-2 lg:px-4">
+        <div class="items-center text-orange-500 leading-none flex lg:inline-flex" role="alert">
+          <span class="font-semibold mr-2 text-left flex-auto">Visit the Awesome <span class="underline">Davide Santagelo</span> GitHub Profile </span>
+          <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+        </div>
+      </div>
+    </a>
     <div class="flex mt-20">
       <div class="w-1/5 h-12"></div>
-      <div class="w-3/5 bg-gray-400 h-12">
-        <div class="mx-auto">
-          <div class="w-2 bg-gray-800"></div>
-          <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-black rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      
+      <div class="w-3/5 bg-gray-400 h-16 mx-auto">
+       
+        <div class="w-2 bg-gray-800"></div>
+          <div class="bg-black p-8 flex flex-col justify-between leading-normal">
             <div class="mb-8">
               <div class="text-orange-500 font-bold text-3xl mb-2">{{ response.name }}</div>
-              <p class="text-gray-200 text-base">{{ response.bio }}</p>
+              <p class="text-gray-200 text-xl">{{ response.bio }}</p>
             </div>
-             <div class="flex items-center">
-              <img class="w-12 h-12 rounded-full mr-4" :src="response.avatar_url">
-              <div class="text-sm">
+            <div class="flex items-center">
+              <img class="w-16 h-16 rounded-full mr-4" :src="response.avatar_url">
+              <div class="text-base">
                 <p class="text-gray-200">{{ response.location }}</p>
-                <a v-bind:href="response.bio" class="text-gray-200">{{ response.blog }}</a>
+                <a v-bind:href="response.bio" class="text-blue-200">{{ response.blog }}</a>
               </div>
             </div>
           </div>
-        </div>
-        
-        <div class="flex">
-          <div class="w-1/6 bg-gray-400 h-10"></div>
-          <div class="w-1/6 bg-gray-500 h-10"></div>
-          <div class="w-1/6 bg-gray-400 h-10"></div>
-          <div class="w-1/6 bg-gray-500 h-10"></div>
-          <div class="w-1/6 bg-gray-400 h-10"></div>
-          <div class="w-1/6 bg-gray-500 h-10"></div>
-        </div>
 
-        <div class="mt-10 flex  flex-wrap min-w-full">
-          <a class="px-4 py-2 mt-2 text-lg text-gray-900 bg-gray-200 rounded-lg sm:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-300" href="#"><span class="badge mb-3 bg-red-800 rounded-full px-2 py-1 text-center object-right-top text-white text-sm mr-1">24</span>Messages</a>
-        </div>
+          <div class="flex">
+            <div class="w-1/6 bg-orange-400 h-10"></div>
+            <div class="w-1/6 bg-orange-500 h-10"></div>
+            <div class="w-1/6 bg-orange-400 h-10"></div>
+            <div class="w-1/6 bg-orange-500 h-10"></div>
+            <div class="w-1/6 bg-orange-400 h-10"></div>
+            <div class="w-1/6 bg-orange-500 h-10"></div>
+          </div>
 
+          
       </div>
+
       <div class="w-1/5 h-12"></div>
     </div>
-    
   </div>
 </template>
 
